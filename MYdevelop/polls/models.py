2f05a232e918c473753239a45d4a4b580d3dd972
Dodiadevyani <devd0093@gmail.com>
2016-11-30 16:02:50 +0530

    Update models.py

--- MYdevelop/polls/models.py
+++ MYdevelop/polls/models.py
@@ -3,7 +3,6 @@ from django.db import models
 from django.utils import timezone
 from django.utils.encoding import python_2_unicode_compatible
 
-@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
@@ -13,7 +12,6 @@ class Question(models.Model):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
-@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
